1b6084fb566214cee41ddae2d12a5d65125d4bd9
laurence añasco <lanasco@jairosoftcom.onmicrosoft.com>
2024-07-17 14:26:05 +0800

    Added fullscreen mode, and disabling the popup

--- FINAL v1/Complete v1.1.py
+++ FINAL v1/Complete v1.1.py
@@ -19,7 +19,7 @@ chromedriver_path = '/usr/bin/chromedriver'
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')  # Required for running as root in Linux
 chrome_options.add_argument('--disable-dev-shm-usage')  # Required for running as root in Linux
-
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
 # Initialize webdriver
 driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=chrome_options)
 
